6856c324eacaab9c6085a932648755a1137ed8a9
Eric Garcia <erraga09@gmail.com>
2021-12-28 21:02:11 -0700

    change folder name

--- index.js
+++ index.js
@@ -5,6 +5,8 @@ const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 const helmet = require("helmet");
 const morgan = require("morgan");
+const userRoute = require("./routes/users");
+const authRoute = require("./routes/auth");
 
 dotenv.config({path:__dirname+'/.env'});
 
@@ -14,6 +16,14 @@ mongoose.connect(
     console.log("Connected to MongoDB");
     }
 );
+
+//middleware
+app.use(express.json());
+app.use(helmet());
+app.use(morgan("common"));
+
+app.use("/api/users", userRoute);
+app.use("/api/auth", authRoute);
 
 app.listen(8800, () => {
     console.log("Backend server is running!");
