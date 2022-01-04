7c400974b1e4b2041263d7b88fa707827033989a
Eric Garcia <erraga09@gmail.com>
2022-01-03 18:07:21 -0700

    like and dislike post and get post completed

--- routes/posts.js
+++ routes/posts.js
@@ -42,8 +42,33 @@ router.delete("/:id", async (req, res) => {
     }
 });
 
-//like a post
+//like and dislike a post
+router.put("/:id/like", async (req, res) => {
+    try {
+        const post = await Post.findById(req.params.id);
+        if (!post.likes.includes(req.body.userId)) {
+            await post.updateOne({ $push: { likes: req.body.userId } });
+            res.status(200).json("The post has been liked")
+        } else  {
+            await post.updateOne({ $pull: { likes: req.body.userId } });
+            res.status(200).json("The post has been disliked")
+        }
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
+
 //get a post
+router.get("/:id", async (req, res) => {
+    try {
+        const post = await Post.findById(req.params.id);
+        res.status(200).json(post);
+    } catch (err) {
+        res.status(500).json(err)
+    }
+});
+
 //get timeline posts
+
 
 module.exports = router;
